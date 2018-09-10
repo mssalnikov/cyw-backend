7f2b3474a65e729d88f03bb08119a629495ffa27
Alexander Chernikov <frankich@mail.ru>
2018-09-10 16:56:38 +0300

    fix migration

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -1,14 +1,36 @@
 -- +goose Up
+create database CYW ENCODING 'UTF-8' OWNER postgres;
+grant all on database CYW to postgres;
+
 CREATE TABLE users (
     id int NOT NULL PRIMARY KEY,
-    username text,
+    fb_uid text,
     name text,
-    surname text
+    last_name text
 );
 
-INSERT INTO users VALUES
-(0, 'root', '', ''),
-(1, 'vojtechvitek', 'Vojtech', 'Vitek');
+CREATE TABLE events (
+    id int NOT NULL PRIMARY KEY,
+    type text,
+    user_id int references users(id)
+);
+
+CREATE TABLE points (
+    id int NOT NULL PRIMARY KEY,
+    event_id int references events(id),
+    point geography(POINT),
+    description text,
+    have_question boolean,
+    question text,
+    answer text,
+    is_chainded boolean,
+    next_point int
+);
+-- INSERT INTO users VALUES
+-- (0, 'root', '', ''),
+-- (1, 'vojtechvitek', 'Vojtech', 'Vitek');
 
 -- +goose Down
+DROP TABLE points;
+DROP TABLE events;
 DROP TABLE users;
